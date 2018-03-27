c80d05aa26099ce82be5cc574dc140654973c51d
stf_ddias <stf_ddias@FASTA0HG4T2J2-D.alelo.elopar.corp>
2018-03-27 17:26:13 -0300

    add novo grupo serviço

---
+++ src/main/java/br/com/syshealth/commons/enums/GrupoServicoEnum.java
@@ -0,0 +1,30 @@
+package br.com.syshealth.commons.enums;
+
+public enum GrupoServicoEnum {
+	
+	CONSULTA(1, "Consultas"), 
+	PRONTO_SOCORRO(1, "Pronto Socorro"), 
+	EXAMES_SIMPLES(1, "Exames Simples"), 
+	EXAMES_ESPECIAIS(1, "Exames Especiais"), 
+	INTENACAO(1, "Internação"), 
+	HONORARIO(1, "Honorário"), 
+	TERAPIA(1, "Terapia"), 
+	OUTROS(1, "Outros");
+
+	private final Integer id;
+	private final String descricao;
+
+	private GrupoServicoEnum(Integer id, String descricao) {
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+}
