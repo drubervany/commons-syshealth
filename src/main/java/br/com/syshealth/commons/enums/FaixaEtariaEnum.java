c02855d6a94badf992e41aff4e44e4b4ea5a9b2b
stf_ddias <stf_ddias@FASTA0HG4T2J2-D.alelo.elopar.corp>
2018-04-11 10:15:17 -0300

    Ajuste idades faixa etaria

--- src/main/java/br/com/syshealth/commons/enums/FaixaEtariaEnum.java
+++ src/main/java/br/com/syshealth/commons/enums/FaixaEtariaEnum.java
@@ -3,9 +3,14 @@ package br.com.syshealth.commons.enums;
 public enum FaixaEtariaEnum {
 
     FAIXA_0_18(0, 18),
-    FAIXA_19_24(19, 24),
-    FAIXA_25_39(25, 39),
-    FAIXA_FINAL(39, 999);
+    FAIXA_19_23(19, 23),
+    FAIXA_24_28(24, 28),
+    FAIXA_29_38(29, 38),
+    FAIXA_39_43(39, 43),
+    FAIXA_44_48(44, 48),
+    FAIXA_49_53(49, 53),
+    FAIXA_54_58(54, 58),
+    FAIXA_FINAL(59, 999);
 
     private int idadeInicio;
 
